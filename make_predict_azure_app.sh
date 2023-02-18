26535c3865c72b2e5389f266135f4b90399162b0
pgpillai <pgpillai@yahoo.com>
2023-02-18 19:16:43 +0000

    added app url to predict script

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://housing02052023.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
